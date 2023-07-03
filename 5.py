634995d3ae1bd42beb481a5f457a4514101cf14b
ka-anna <134880993+ka-anna@users.noreply.github.com>
2023-07-04 00:21:59 +0400

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,5 +1,6 @@
-//Create a global variable called counter and initialize it with 0.
-//Write a function called increment_counter that increments the value of counter by 1 and prints the updated value. Call the function multiple times and observe the changes in the global variable.
+#Create a global variable called counter and initialize it with 0.
+#Write a function called increment_counter that increments the value of counter by 1 and prints the updated value.
+#Call the function multiple times and observe the changes in the global variable.
 
 
 counter = 0
