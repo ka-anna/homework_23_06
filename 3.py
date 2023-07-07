45099244000c2b5a1a13e83554c4170e7aedc67b
ka-anna <134880993+ka-anna@users.noreply.github.com>
2023-07-07 13:49:12 +0400

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,10 +1,10 @@
 #Write a function called is_sorted that is given a list 
 #and returns True if the list is sorted and False otherwise. Do this without using the sort method.
 
-ls = [1,8,6]
+ls = [5,9,7]
 def is_sorted(ls):
-    if sorted(ls)==ls:
-        print("True")
-    else:
-        print("False")
-is_sorted(ls)
+ for i in range (len(ls)-1):
+    if ls[i+1] <= ls[i] and ls[-(i+1)] >= ls[-(i+2)]:
+        return False
+ return True
+print(is_sorted(ls))
